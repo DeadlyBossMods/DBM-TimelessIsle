b925c5125a8de70e1c7413b2546f0fe3c04dc746
Adam <MysticalOS@users.noreply.github.com>
2014-08-25 20:11:42 +0000

    Initial Commit

---
+++ localization.en.lua
@@ -0,0 +1,20 @@
+local L
+
+L = DBM:GetModLocalization("TimelessIsle")
+
+L:SetGeneralLocalization({
+	name = "World: Timeless Isle"
+})
+
+L:SetWarningLocalization({
+	specWarnShip	= "Ship Summoned!"
+})
+
+L:SetOptionLocalization({
+	specWarnShip	= "Show special warning when Dread Ship Vazuvius is summmoned",
+	StrictFilter	= "Filter casts from all mobs except for current target/focus"
+})
+
+L:SetMiscLocalization({
+	shipSummon		= "Hahahahaha!"
+})
