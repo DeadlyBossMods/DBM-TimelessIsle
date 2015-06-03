40d73a623b48aa4c1a19e0dc612e90359838d4a8
Adam <MysticalOS@users.noreply.github.com>
2015-06-03 04:56:34 +0000

    Kill off per boss "SpeedKillTimer"

--- IsleTimeless.lua
+++ IsleTimeless.lua
@@ -52,7 +52,6 @@ local specWarnRenewingMists		= mod:NewSpecialWarningInterrupt(147769)
 
 mod:AddBoolOption("StrictFilter", true)--Only warn for current target/focus and nothing else. Otherwise you run risk of excessive spam when fighting near other groups fighting same mobs.
 mod:RemoveOption("HealthFrame")
-mod:RemoveOption("SpeedKillTimer")
 
 local GetCurrentMapAreaID, SetMapToCurrentZone = GetCurrentMapAreaID, SetMapToCurrentZone
 local UnitAffectingCombat, UnitGUID = UnitAffectingCombat, UnitGUID
